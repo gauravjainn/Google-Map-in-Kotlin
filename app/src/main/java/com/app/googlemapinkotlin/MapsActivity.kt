94895fd2a9fcfb2068ad16fe04649df9b660d8ea
Gaurav Jain <gaurav.jain@nxgn.local>
2019-10-24 15:11:51 +0530

    added current location and zoom functionality in google map

--- app/src/main/java/com/app/googlemapinkotlin/MapsActivity.kt
+++ app/src/main/java/com/app/googlemapinkotlin/MapsActivity.kt
@@ -1,8 +1,8 @@
 package com.app.googlemapinkotlin
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -10,34 +10,38 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
+
+
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        /*to show zoom in zoom out button in google map*/
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        /*to show current location on google map*/
+        mMap.isMyLocationEnabled = true
+         /*Add a marker to given location  and move the camera*/
+        val latLng = LatLng(37.0902, 95.7129)
+        mMap.addMarker(MarkerOptions().position(latLng).title("united states"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10F));
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(33.8688, 151.2093)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Sydeney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
     }
+
+
+
+
 }
